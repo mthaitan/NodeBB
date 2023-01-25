eb4619bf25f78d9f4ce86c5f6e4d528b8d937b6f
Maddie Thai-Tang <mthaitan@andrew.cmu.edu>
2023-01-25 11:59:04 -0500

    type adjustments

--- src/posts/bookmarks.js
+++ src/posts/bookmarks.js
@@ -1,95 +1,68 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+'use strict';
+
+const db = require('../database');
+const plugins = require('../plugins');
+
+module.exports = function (Posts) {
+    Posts.bookmark = async function (pid, uid) {
+        return await toggleBookmark('bookmark', pid, uid);
+    };
+
+    Posts.unbookmark = async function (pid, uid) {
+        return await toggleBookmark('unbookmark', pid, uid);
+    };
+
+    async function toggleBookmark(type, pid, uid) {
+        if (parseInt(uid, 10) <= 0) {
+            throw new Error('[[error:not-logged-in]]');
+        }
+
+        const isBookmarking = type === 'bookmark';
+
+        const [postData, hasBookmarked] = await Promise.all([
+            Posts.getPostFields(pid, ['pid', 'uid']),
+            Posts.hasBookmarked(pid, uid),
+        ]);
+
+        if (isBookmarking && hasBookmarked) {
+            throw new Error('[[error:already-bookmarked]]');
+        }
+
+        if (!isBookmarking && !hasBookmarked) {
+            throw new Error('[[error:already-unbookmarked]]');
+        }
+
+        if (isBookmarking) {
+            await db.sortedSetAdd(`uid:${uid}:bookmarks`, Date.now(), pid);
+        } else {
+            await db.sortedSetRemove(`uid:${uid}:bookmarks`, pid);
+        }
+        await db[isBookmarking ? 'setAdd' : 'setRemove'](`pid:${pid}:users_bookmarked`, uid);
+        postData.bookmarks = await db.setCount(`pid:${pid}:users_bookmarked`);
+        await Posts.setPostField(pid, 'bookmarks', postData.bookmarks);
+
+        plugins.hooks.fire(`action:post.${type}`, {
+            pid: pid,
+            uid: uid,
+            owner: postData.uid,
+            current: hasBookmarked ? 'bookmarked' : 'unbookmarked',
+        });
+
+        return {
+            post: postData,
+            isBookmarked: isBookmarking,
+        };
+    }
+
+    Posts.hasBookmarked = async function (pid, uid) {
+        if (parseInt(uid, 10) <= 0) {
+            return Array.isArray(pid) ? pid.map(() => false) : false;
+        }
+
+        if (Array.isArray(pid)) {
+            const sets = pid.map(pid => `pid:${pid}:users_bookmarked`);
+            return await db.isMemberOfSets(sets, uid);
+        }
+        return await db.isSetMember(`pid:${pid}:users_bookmarked`, uid);
+    };
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = __importDefault(require("../database"));
-const plugins_1 = __importDefault(require("../plugins"));
-const data_1 = require("./data");
-function default_1(Posts) {
-    function hasBeenBookmarked(pid, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (parseInt(uid, 10) <= 0) {
-                return Array.isArray(pid) ? pid.map(() => false) : false;
-            }
-            if (Array.isArray(pid)) {
-                const sets = pid.map(pid => `pid:${pid}:users_bookmarked`);
-                // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                return yield database_1.default.isMemberOfSets(sets, uid);
-            }
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            return yield database_1.default.isSetMember(`pid:${pid}:users_bookmarked`, uid);
-        });
-    }
-    function toggleBookmark(type, pid, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (parseInt(uid, 10) <= 0) {
-                throw new Error('[[error:not-logged-in]]');
-            }
-            const isBookmarking = type === 'bookmark';
-            const [postData, hasBookmarked] = yield Promise.all([
-                // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                (0, data_1.getPostFields)(pid, ['pid', 'uid']),
-                hasBeenBookmarked(pid, uid),
-            ]);
-            if (isBookmarking && hasBookmarked) {
-                throw new Error('[[error:already-bookmarked]]');
-            }
-            if (!isBookmarking && !hasBookmarked) {
-                throw new Error('[[error:already-unbookmarked]]');
-            }
-            if (isBookmarking) {
-                // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                yield database_1.default.sortedSetAdd(`uid:${uid}:bookmarks`, Date.now(), pid);
-            }
-            else {
-                // The next line calls a function in a module that has not been updated to TS yet
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-                yield database_1.default.sortedSetRemove(`uid:${uid}:bookmarks`, pid);
-            }
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield database_1.default[isBookmarking ? 'setAdd' : 'setRemove'](`pid:${pid}:users_bookmarked`, uid);
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            postData.bookmarks = yield database_1.default.setCount(`pid:${pid}:users_bookmarked`);
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-            yield (0, data_1.setPostField)(pid, 'bookmarks', postData.bookmarks);
-            plugins_1.default.hooks.fire(`action:post.${type}`, {
-                pid: pid,
-                uid: uid,
-                owner: postData.uid,
-                current: hasBookmarked ? 'bookmarked' : 'unbookmarked',
-            });
-            return {
-                post: postData,
-                isBookmarked: isBookmarking,
-            };
-        });
-    }
-    function bookmark(pid, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield toggleBookmark('bookmark', pid, uid);
-        });
-    }
-    function unbookmark(pid, uid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield toggleBookmark('unbookmark', pid, uid);
-        });
-    }
-}
-exports.default = default_1;
--- src/posts/bookmarks.ts
+++ src/posts/bookmarks.ts
@@ -5,35 +5,31 @@ import { PostObject } from '../types';
 import { getPostFields, setPostField } from './data';
 
 export default function (Posts:PostObject) {
-    async function hasBeenBookmarked(pid:number[] | number, uid:string) {
+    async function hasBeenBookmarked(pid:number[] | number, uid:string):Promise<false|boolean[]> {
         if (parseInt(uid, 10) <= 0) {
             return Array.isArray(pid) ? pid.map(() => false) : false;
         }
 
         if (Array.isArray(pid)) {
             const sets = pid.map(pid => `pid:${pid}:users_bookmarked`);
-            // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             return await db.isMemberOfSets(sets, uid);
         }
-        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         return await db.isSetMember(`pid:${pid}:users_bookmarked`, uid);
     }
 
-    async function toggleBookmark(type:string, pid:number, uid:string) {
+    async function toggleBookmark(type:string, pid:number, uid:string)
+        :Promise<{post: PostObject; isBookmarked: boolean;}> {
         if (parseInt(uid, 10) <= 0) {
             throw new Error('[[error:not-logged-in]]');
         }
 
         const isBookmarking = type === 'bookmark';
 
-        // The next line calls a function in a module that has not been updated to TS yet
+        const hasBookmarked:Promise<false|boolean[]> = hasBeenBookmarked(pid, uid);
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-        const [postData, hasBookmarked]:[PostObject, Promise<any>] = await Promise.all([
-            getPostFields(pid, ['pid', 'uid']),
-            hasBeenBookmarked(pid, uid),
-        ]);
+        const postData:PostObject = getPostFields(pid, ['pid', 'uid']);
 
         if (isBookmarking && hasBookmarked) {
             throw new Error('[[error:already-bookmarked]]');
@@ -44,21 +40,16 @@ export default function (Posts:PostObject) {
         }
 
         if (isBookmarking) {
-            // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             await db.sortedSetAdd(`uid:${uid}:bookmarks`, Date.now(), pid);
         } else {
-            // The next line calls a function in a module that has not been updated to TS yet
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             await db.sortedSetRemove(`uid:${uid}:bookmarks`, pid);
         }
-        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         await db[isBookmarking ? 'setAdd' : 'setRemove'](`pid:${pid}:users_bookmarked`, uid);
-        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         postData.bookmarks = await db.setCount(`pid:${pid}:users_bookmarked`);
-        // The next line calls a function in a module that has not been updated to TS yet
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
         await setPostField(pid, 'bookmarks', postData.bookmarks);
 
@@ -75,11 +66,11 @@ export default function (Posts:PostObject) {
         };
     }
 
-    async function bookmark(pid:number, uid:string) {
+    async function bookmark(pid:number, uid:string):Promise<{ post:PostObject; isBookmarked:boolean; }> {
         return await toggleBookmark('bookmark', pid, uid);
     }
 
-    async function unbookmark(pid:number, uid:string) {
+    async function unbookmark(pid:number, uid:string):Promise<{ post: PostObject; isBookmarked: boolean; }> {
         return await toggleBookmark('unbookmark', pid, uid);
     }
 }
